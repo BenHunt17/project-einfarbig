e6c2ad3620fb86fca6d39ba59d005e264f0009a4
BenHunt17 <58478479+BenHunt17@users.noreply.github.com>
2023-10-29 16:37:55 +0000

    Finalise basic infrastructure for cpu fetch, decode, execute
    Quite happy with it now. The operand enum corresponds to the cpu instruction sheet im using which seems sensible.
    
    Also can hopefully implement singular functions for each instruction type i.e. ld

--- dmg-emulator/src/cpu.c
+++ dmg-emulator/src/cpu.c
@@ -2,10 +2,6 @@
 
 #include "cpu.h"
 
-Instruction instructionSet[256] = {
-	{ ld, 4, NULL }
-};
-
 void initialiseCpuState(CpuState* cpuState) {
 	cpuState->pc = 0x100;
 	cpuState->sp = 0xfffe;
@@ -15,6 +11,20 @@ void initialiseCpuState(CpuState* cpuState) {
 
 void freeCpuState(CpuState* cpuState) {
 	free(cpuState);
+}
+
+uint8_t fetch8(Bus* bus) {
+	uint8_t byte = readByte(bus, bus->cpu->pc);
+	bus->cpu->pc++;
+
+	return byte;
+}
+
+uint16_t fetch16(Bus* bus) {
+	uint8_t lowByte = fetch8(bus);
+	uint8_t highByte = fetch8(bus);
+
+	return (highByte << 8) | lowByte;
 }
 
 bool getFlag(CpuState* state, uint8_t flagBit) {
@@ -30,6 +40,10 @@ void setFlag(CpuState* state, uint8_t flagBit, bool isSet) {
 	}
 }
 
+Instruction instructionSet[256] = {
+	{ nop, 4, OPERAND_NONE, OPERAND_NONE }, { ld, 12, OPERAND_BC, OPERAND_N16 }
+};
+
 void cpuCycle(Bus* bus) {
 	CpuState* cpuState = bus->cpu;
 
@@ -37,10 +51,12 @@ void cpuCycle(Bus* bus) {
 		Instruction instruction = instructionSet[readByte(bus, cpuState->pc)];
 		cpuState->pc++;
 
-		instruction.getOperands(bus);
-		instruction.function(bus);
+		cpuState->operandA = instruction.operandA;
+		cpuState->operandB = instruction.operandB;
+		instruction.execute(bus);
 
 		cpuState->instructionCycleCount = instruction.clockCycles;
+		cpuState->elapsedInstructionCycles++;
 	}
 	else {
 		cpuState->elapsedInstructionCycles++;
@@ -50,3 +66,10 @@ void cpuCycle(Bus* bus) {
 	}
 }
 
+void nop(Bus* bus) {
+	//Do nothing
+}
+
+void ld(Bus* bus) {
+	//TODO - implement
+}
--- dmg-emulator/src/cpu.h
+++ dmg-emulator/src/cpu.h
@@ -12,6 +12,26 @@
 #define HALF_CARRY_FLAG_BIT  5
 #define CARRY_FLAG_BIT       4
 
+typedef enum {
+	OPERAND_NONE,
+	OPERAND_N8,
+	OPERAND_N16,
+	OPERAND_A8,
+	OPERAND_A16,
+	OPERAND_E8,
+	OPERAND_A,
+	OPERAND_B,
+	OPERAND_C,
+	OPERAND_D,
+	OPERAND_E,
+	OPERAND_H,
+	OPERAND_L,
+	OPERAND_BC,
+	OPERAND_DE,
+	OPERAND_HL,
+	OPERAND_SP,
+} Operand;
+
 typedef struct {
 	uint16_t pc;
 	uint8_t registers[NUMBER_OF_GENERAL_REGISTERS];
@@ -19,22 +39,31 @@ typedef struct {
 
 	uint8_t instructionCycleCount;
 	uint8_t elapsedInstructionCycles;
+	uint16_t operandA;
+	uint16_t operandB;
 } CpuState;
 
 typedef struct {
-	void (*function)(Bus* bus);
+	void (*execute)(Bus* bus);
 	uint8_t clockCycles;
-	void (*getOperands)(Bus* bus);
+	Operand operandA;
+	Operand operandB;
 } Instruction;
 
 void initialiseCpuState(CpuState* cpuState);
 
 void freeCpuState(CpuState* cpuState);
 
+uint8_t fetch8(Bus* bus);
+
+uint16_t fetch16(Bus* bus); //Little endian format
+
 bool getFlag(CpuState* state, uint8_t flagBit);
 
 void setFlag(CpuState* state, uint8_t flagBit, bool isSet);
 
 void cpuCycle(Bus* bus);
+
+void nop(Bus* bus);
 
 void ld(Bus* bus);
